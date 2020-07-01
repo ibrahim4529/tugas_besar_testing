f44fdc41510f520316137dd42405c51f559228d1
Ibrahim hanif <ibrahimhanif@student.polindra.ac.id>
2020-07-01 13:41:11 +0700

    Change Type Of Testing From future to unit

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Unit;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -17,6 +17,6 @@ class UserTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertRedirect();
     }
 }
